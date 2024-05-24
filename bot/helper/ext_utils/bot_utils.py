2091d060dffbd9224ff8a69de2ec502fba771a65
Anshuman Sahu <ansuman1705@gmail.com>
2024-05-24 05:56:19 +0000

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -136,16 +136,19 @@ def handleIndex(index, dic):
         else: break
     return index
 
+
 def get_progress_bar_string(pct):
     pct = float(str(pct).strip('%'))
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
     cPart = int(p % 8 - 1)
-    p_str = '■' * cFull
+    p_str = '⬢' * cFull
     if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
+        p_str += '⬡', '⬡', '⬡', '⬡', '⬡', '⬡', '⬢'[cPart]
+    p_str += '⬡' * (12 - cFull)
+    return f"{p_str}"
+
+
 
 
 def get_all_versions():
