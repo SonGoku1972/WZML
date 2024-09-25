56021ac487ccd7e42b39421eb9f324ebdd829a03
SonGoku1972 <169218549+SonGoku1972@users.noreply.github.com>
2024-09-25 19:30:13 +0530

    Update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -1212,8 +1212,7 @@ async def edit_bot_settings(client, query):
         await query.answer(f'{value}', show_alert=True)
     elif data[2] in ['DATABASE_URL', 'TELEGRAM_API', 'TELEGRAM_HASH', 'UPSTREAM_REPO', 'USER_SESSION_STRING', 'MEGA_PASSWORD'] and not await CustomFilters.owner(client, query):
         await query.answer('Only owner can view this!', show_alert=True)
-        return
-    if len(str(value)) > 500:
+        if len(str(value)) > 500:
             await query.answer()
             with BytesIO(str.encode(value)) as out_file:
                 out_file.name = f"{data[2]}.txt"
